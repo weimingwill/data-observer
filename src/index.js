1f25bba46587e0969ec6f7fa3fa1cc57dce9906f
Weiming <wingalong@gmail.com>
2017-05-25 19:04:08 +0800

    Observe array and record path

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 
 import { 
 	isPlainObject,
-
+	isPlainArray
 } from './helper.js'
 
 const OVERRIDE_ARRAY_METHODS = ['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse']
@@ -18,41 +18,73 @@ export class dataObserver {
 		this.observe(obj)
 	}
  	
-	observe(obj) {
-		Object.keys(obj).forEach((key, index) => {
+	observe(obj, path) {
+		if (isPlainArray(obj)) {
+			this.overrideArrayPrototype(obj, path)
+		}
+
+		Object.keys(obj).forEach(key => {
+			let pathArray = path && path.slice()
+			if (pathArray) {
+				pathArray.push(key)
+			} else {
+				pathArray = [key]
+			}
 			let oldVal = obj[key]
 			Object.defineProperty(obj, key, {
 				get: () => {
 					return oldVal
 				},
 
-				set: ((newVal) => {
-					this.$callback(newVal, oldVal)
-					// oldVal = newVal
-				}).bind(this)
+				set: (newVal) => {
+					if (oldVal !== newVal) {
+						if (isPlainObject(newVal) || isPlainArray(newVal)) {
+							this.observe(newVal, pathArray)
+						}
+						this.$callback(newVal, oldVal, pathArray)
+					}	
+				}
 			})
 		
-			if (isPlainObject(obj[key])) {
-				this.observe(obj[key])
+			if (isPlainObject(obj[key]) || isPlainArray(obj[key])) {
+				this.observe(obj[key], pathArray)
 			}
-		})
+		}, this)
 	}
 
-	overrideArrayPrototype (array) {
+	overrideArrayPrototype (array, path) {
 		let originProto = Array.prototype,
 			overrideProto = Object.create(Array.prototype),
-			_this = this;
+			_this = this,
+			result
 
-		Object.keys(OVERRIDE_ARRAY_METHODS).forEach(method => {
+		Object.keys(OVERRIDE_ARRAY_METHODS).forEach((key, index) => {
+			let method = OVERRIDE_ARRAY_METHODS[index]
+			let oldArray = []
+
 			Object.defineProperty(overrideProto, method, {
-				value: () => {
-					
+				// cannot use arrow function here, need to bind `this`
+				value: function() {
+					oldArray = this.slice()
+
+					let arg = [].slice.apply(arguments)
+
+					result = originProto[method].apply(this, arg)
+
+					_this.observe(this, path)
+
+					_this.$callback(this, oldArray, path)
+
+					return result
 				},
 				writable: true,
 				enumerable: false,
 				configurable: true
 			})
-		});
+		}, this)
+
+		array.__proto__ = overrideProto
+
 
 	}
 }
